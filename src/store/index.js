d8572d28ff5890e302cf9d6f52b11f33c6592692
sweetbambie <sophia@miao.fm>
2024-12-12 13:34:44 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 
 export const useRegistrationStore = defineStore('registration', () => {
   const firstName = ref('');
@@ -8,82 +8,35 @@ export const useRegistrationStore = defineStore('registration', () => {
   const password = ref('');
   const rePassword = ref('');
 
-  const persistData = () => {
-    localStorage.setItem('firstName', firstName.value);
-    localStorage.setItem('lastName', lastName.value);
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('password', password.value);
-  };
-
-  const loadData = () => {
-    firstName.value = localStorage.getItem('firstName') || '';
-    lastName.value = localStorage.getItem('lastName') || '';
-    email.value = localStorage.getItem('email') || '';
-    password.value = localStorage.getItem('password') || '';
-  };
-
-  onMounted(() => {
-    loadData();
-  });
-
-  const setRegistrationData = (data) => {
-    firstName.value = data.firstName;
-    lastName.value = data.lastName;
-    email.value = data.email;
-    password.value = data.password;
-    rePassword.value = ''; 
-    persistData(); 
-  };
-
   return {
     firstName,
     lastName,
     email,
     password,
     rePassword,
-    setRegistrationData,
+    setRegistrationData(data) {
+      firstName.value = data.firstName;
+      lastName.value = data.lastName;
+      email.value = data.email;
+      password.value = data.password;
+      rePassword.value = '';
+    },
   };
+}, {
+  persist: true,
 });
 
 export const useStore = defineStore('store', () => {
+  const email = ref("");
   const cart = ref(new Map());
 
-  const persistData = () => {
-    const s = JSON.stringify(Array.from(cart.value.entries()));
-    console.log("Saving cart ", s)
-    localStorage.setItem('cart', JSON.stringify(Array.from(cart.value.entries())));
-  }
-
-  const loadData = () => {
-    const storedCart = localStorage.getItem('cart');
-    if (storedCart) {
-      const parsedCart = JSON.parse(storedCart);
-      // Use cart.value.clear() to update the existing Map instance
-      cart.value.clear();
-      parsedCart.forEach(([key, value]) => {
-        cart.value.set(key, value);
-      });
-    }
-  }
-
-
-  function addToCart(id, movieData) {
-    cart.value.set(id, movieData);
-    persistData()
-  }
-
-  function removeFromCart(id) {
-    cart.value.delete(id);
-    persistData()
-  }
-
-  onMounted(() => {
-      loadData();
-  });
-
-  return {
-    cart,
-    addToCart,
-    removeFromCart,
+  const addToCart = (key, value) => {
+    cart.value.set(key, value);
   };
+
+  const removeFromCart = (key) => {
+    cart.value.delete(key);
+  };
+
+  return { email, cart, addToCart, removeFromCart };
 });
